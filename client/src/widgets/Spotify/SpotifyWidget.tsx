6718e54172a2d1a08891f2e9c3b140c1a81ec9cb
Alok Gupta <alokgupta1560@gmail.com>
2024-07-21 01:15:14 +0530

    Add widgets folder

---
+++ client/src/widgets/Spotify/SpotifyWidget.tsx
@@ -0,0 +1,11 @@
+function SpotifyWidget() {
+  
+
+  return (
+   
+    <iframe className="rounded-r-xl" src="https://open.spotify.com/embed/playlist/2bcSVCNr1AtyVrB3YP9dLl?utm_source=generator" width="100%" height="352"   allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+   
+  );
+}
+
+export default SpotifyWidget;
