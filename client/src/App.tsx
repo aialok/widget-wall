45518d32ed5b9c0b35641bcab001ea46759c6de9
siva222003 <sivapidugu02@gmail.com>
2024-07-21 00:45:48 +0530

    added basic drag-drop

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,37 @@
-import { Button } from "@/components/ui/button";
-import { Home } from "lucide-react";
-import { Route, Routes } from "react-router-dom";
-import About from "./About";
+import { useMemo } from "react";
+import { Responsive, WidthProvider } from "react-grid-layout";
+import { HomeLayouts, keys } from "./utils/layout.helper";
 export default function App() {
-  return (
-    <main>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
+  const ResponsiveReactGridLayout = useMemo(() => WidthProvider(Responsive), []);
 
-      <Button>Click me</Button>
+  return (
+    <main className="bg-[#f7f2f2]">
+      <div className="w-[1280px] m-auto flex justify-between b-10">
+        <ResponsiveReactGridLayout
+          className="m-auto w-[900px]"
+          breakpoints={{ xl: 1200, lg: 899, md: 768, sm: 480, xs: 200 }}
+          cols={{ xl: 3, lg: 3, md: 2, sm: 1, xs: 1 }}
+          rowHeight={300}
+          layouts={HomeLayouts}
+        >
+          {keys.map((key) => (
+            <div
+              key={key}
+              className="bg-[#f5f5f7] flex justify-center items-center shadow-[inset_0_0_0_2px_rgba(0,0,0,0)] rounded-2xl text-2xl text-[#1d1d1f] visible cursor-grab active:cursor-grabbing"
+            >
+              <Block keyProp={key} />
+            </div>
+          ))}
+        </ResponsiveReactGridLayout>
+      </div>
     </main>
   );
 }
+
+const Block = ({ keyProp }: { keyProp: string }) => {
+  return (
+    <div className="h-full w-full flex flex-col justify-center items-center p-6 bg-white text-[var(--black-1)] rounded-2xl text-3xl uppercase">
+      {keyProp}
+    </div>
+  );
+};
