8c6149d8471d16d2f4a1e12e0be8232da30ed275
Alok Gupta <alokgupta1560@gmail.com>
2024-07-21 23:48:49 +0530

    Added annoucement with backend

--- client/src/widgets/Announcements/Announcements.tsx
+++ client/src/widgets/Announcements/Announcements.tsx
@@ -1,5 +1,6 @@
 import { cn } from "@/lib/utils";
 import { AnimatedList } from "@/components/magicui/animated-list";
+import { useEffect, useState } from "react";
 
 interface Item {
   name: string;
@@ -43,7 +44,13 @@ let notifications = [
 
 notifications = Array.from({ length: 10 }, () => notifications).flat();
 
-const Notification = ({ name, description, icon, color, time }: Item) => {
+const Notification = (props : any) => {
+  // const fetchData = async () => {
+  //   const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  //   const data = await res.json();
+  //   console.log(data
+  // }
+
   return (
     <figure
       className={cn(
@@ -53,26 +60,26 @@ const Notification = ({ name, description, icon, color, time }: Item) => {
         // light styles
         "bg-white [box-shadow:0_0_0_1px_rgba(0,0,0,.03),0_2px_4px_rgba(0,0,0,.05),0_12px_24px_rgba(0,0,0,.05)]",
         // dark styles
-        "transform-gpu dark:bg-transparent dark:backdrop-blur-md dark:[border:1px_solid_rgba(255,255,255,.1)] dark:[box-shadow:0_-20px_80px_-20px_#ffffff1f_inset]",
+        "transform-gpu dark:bg-transparent dark:backdrop-blur-md dark:[border:1px_solid_rgba(255,255,255,.1)] dark:[box-shadow:0_-20px_80px_-20px_#ffffff1f_inset]"
       )}
     >
       <div className="flex flex-row items-center gap-3">
         <div
           className="flex size-10 items-center justify-center rounded-2xl"
           style={{
-            backgroundColor: color,
+            backgroundColor:  "#1E86FF",
           }}
         >
-          <span className="text-lg">{icon}</span>
+          <span className="text-lg">{"💬"}</span>
         </div>
         <div className="flex flex-col overflow-hidden">
           <figcaption className="flex flex-row items-center whitespace-pre text-lg font-medium dark:text-white ">
-            <span className="text-sm sm:text-lg">{name}</span>
+            <span className="text-sm sm:text-lg">{"Annoucement"}</span>
             <span className="mx-1">·</span>
-            <span className="text-xs text-gray-500">{time}</span>
+            <span className="text-xs text-gray-500">{"5m ago"}</span>
           </figcaption>
           <p className="text-sm font-normal dark:text-white/60">
-            {description}
+            {props.name}
           </p>
         </div>
       </div>
@@ -80,21 +87,34 @@ const Notification = ({ name, description, icon, color, time }: Item) => {
   );
 };
 
-export default function Announcements({
-  className,
-}: {
-  className?: string;
-}) {
+export default function Announcements({ className }: { className?: string }) {
+  const [announcement, setAnnouncement] = useState<string[]>([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/api/v1/announcement")
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res.data);
+        setAnnouncement(res.data);
+      });
+  }, []);
+
+  /*
+
+
+
+  */
+
   return (
     <div
       className={cn(
         "relative flex h-[500px] w-full flex-col p-6 overflow-hidden rounded-lg border bg-background md:shadow-xl",
-        className,
+        className
       )}
     >
       <AnimatedList>
-        {notifications.map((item, idx) => (
-          <Notification {...item} key={idx} />
+        {announcement.map((item, idx) => (
+          <Notification name={item.description} key={idx} />
         ))}
       </AnimatedList>
     </div>
